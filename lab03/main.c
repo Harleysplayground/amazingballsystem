005171aeb9e01e3fc75f480db607e7b9e742d7ec
csun12 <csun12@a2f417b5-1eaa-4f03-aa2b-86c8419867a1>
2014-09-27 23:11:00 +0000

    serial functions good

--- lab03/main.c
+++ lab03/main.c
@@ -25,15 +25,14 @@ _FGS(GCP_OFF);
 int timeout = 0;
 int error = 0;
 
-/*void __attribute__((__interrupt__)) _T1Interrupt(void) {
-
+/*
+void __attribute__((__interrupt__)) _T1Interrupt(void) {
     timeout = 1;
     IFS0bits.T1IF = 0;
-
-}*/
+}
+ */
 
 int main(int argc, char** argv) {
-
     uint16_t crc = 0;
     uint8_t buffer1[4];
     uint8_t buffer2[256];
@@ -44,10 +43,9 @@ int main(int argc, char** argv) {
     lcd_clear();
     lcd_locate(0, 0);
 
+    uart2_init();
 
-    uart2_init(9600);
-
-
+    /*
     __builtin_write_OSCCONL(OSCCONL | 2);
     T1CONbits.TON = 0; //Disable Timer
     T1CONbits.TCS = 1; //Select external clock
@@ -59,23 +57,29 @@ int main(int argc, char** argv) {
     IFS0bits.T1IF = 0; // Clear Timer1 Interrupt Flag
     //   IEC0bits.T1IE = 1; // Enable Timer1 interrupt
     T1CONbits.TON = 1; // Start Timer
+     */
 
-
-
-
-
+    lcd_locate(0, 0);
+    lcd_printf("U2INIT");
+    lcd_locate(0, 1);
     while (1) {
+        char serialbuf = 0;
+        if (uart2_getc(&serialbuf)) {
+            lcd_printf("%x ", serialbuf);
+            uart2_putc(1);
+        }
+    }
+
+    while (0) {
         crc = 0;
 
         while (!uart2_getc(&buffer1[0]));
 
-        TMR1 = 0x00;
-        IEC0bits.T1IE = 1;
+        //TMR1 = 0x00;
+        //IEC0bits.T1IE = 1;
 
         while (!uart2_getc(&buffer1[1]));
-
         while (!uart2_getc(&buffer1[2]));
-
         while (!uart2_getc(&buffer1[3]));
 
         length = buffer1[3];
@@ -84,15 +88,11 @@ int main(int argc, char** argv) {
         for (i = 0; i < length; i++) {
             while (!uart2_getc(&buffer2[i]) && (!timeout));
             crc_update(crc, buffer2[i]);
-
-
         }
-        IEC0bits.T1IE = 0;
+        //IEC0bits.T1IE = 0;
 
-        
         lcd_locate(0, 0);
-        lcd_printf("Errors: %d",error);
-        
+        lcd_printf("Errors: %d", error);
 
         if (timeout == 1) {
             uart2_putc(0);
@@ -109,16 +109,10 @@ int main(int argc, char** argv) {
             continue;
         }
 
-
         uart2_putc(1);
         buffer2[length] = 0;
-        lcd_locate(0, 1);
-        lcd_printf("Message: %s", buffer2);
-
-
+        //lcd_locate(0, 1);
+        //lcd_printf("Message: %s", buffer2);
     }
-
-
-
-    return (0);
+    return(0);
 }
