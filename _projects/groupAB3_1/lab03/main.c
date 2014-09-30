f56438da4433c9667e0df570c00dd0c5df0f019c
csun12 <csun12@a2f417b5-1eaa-4f03-aa2b-86c8419867a1>
2014-09-30 22:25:34 +0000

    lab3 done

--- _projects/groupAB3_1/lab03/main.c
+++ _projects/groupAB3_1/lab03/main.c
@@ -8,6 +8,10 @@
 #include "flexserial.h"
 #include "crc16.h"
 
+#define LED1_TRIS TRISAbits.TRISA4
+#define LED1_PORT PORTAbits.RA4
+#define LED1 4
+
 
 /* Initial configuration by EE */
 // Primary (XT, HS, EC) Oscillator with PLL
@@ -25,12 +29,11 @@ _FGS(GCP_OFF);
 int timeout = 0;
 int error = 0;
 
-/*
 void __attribute__((__interrupt__)) _T1Interrupt(void) {
+    TOGGLELED(LED1_PORT);
     timeout = 1;
     IFS0bits.T1IF = 0;
 }
- */
 
 int main(int argc, char** argv) {
     uint16_t crc = 0;
@@ -39,13 +42,14 @@ int main(int argc, char** argv) {
     uint8_t length;
 
     __C30_UART = 1;
+    CLEARLED(LED1_TRIS);
     lcd_initialize();
     lcd_clear();
     lcd_locate(0, 0);
+    lcd_printf("Welcome to the       AWESOME Lab3 receiverby --GROUP NO.1--")
 
     uart2_init();
 
-    /*
     __builtin_write_OSCCONL(OSCCONL | 2);
     T1CONbits.TON = 0; //Disable Timer
     T1CONbits.TCS = 1; //Select external clock
@@ -55,64 +59,64 @@ int main(int argc, char** argv) {
     PR1 = 32767; //Load the period value
     IPC0bits.T1IP = 0x01; // Set Timer1 Interrupt Priority Level
     IFS0bits.T1IF = 0; // Clear Timer1 Interrupt Flag
-    //   IEC0bits.T1IE = 1; // Enable Timer1 interrupt
-    T1CONbits.TON = 1; // Start Timer
-     */
+    IEC0bits.T1IE = 0; // Enable Timer1 interrupt
+    T1CONbits.TON = 0; // Start Timer
 
-    lcd_locate(0, 0);
-    lcd_printf("U2INIT");
-    lcd_locate(0, 1);
     while (1) {
-        uint8_t serialbuf = 0;
-        if (uart2_getc(&serialbuf)) {
-            lcd_printf("%x ", serialbuf);
-            uart2_putc(1);
-        }
-    }
-
-    while (0) {
         crc = 0;
 
+        /* look for start byte */
         while (!uart2_getc(&buffer1[0]));
+        if (buffer1[0] != 0) continue;
 
-        //TMR1 = 0x00;
-        //IEC0bits.T1IE = 1;
+        TMR1 = 0x00;
+        IEC0bits.T1IE = 1;
+        T1CONbits.TON = 1; // Start Timer
 
-        while (!uart2_getc(&buffer1[1]));
-        while (!uart2_getc(&buffer1[2]));
-        while (!uart2_getc(&buffer1[3]));
+        while (!uart2_getc(&buffer1[1]) && !timeout);
+        while (!uart2_getc(&buffer1[2]) && !timeout);
+        while (!uart2_getc(&buffer1[3]) && !timeout);
 
         length = buffer1[3];
 
-        int i;
+        unsigned int i;
         for (i = 0; i < length; i++) {
-            while (!uart2_getc(&buffer2[i]) && (!timeout));
-            crc_update(crc, buffer2[i]);
+            while (!uart2_getc(&buffer2[i]) && !timeout);
+            if (timeout) break;
+            crc = crc_update(crc, buffer2[i]);
         }
-        //IEC0bits.T1IE = 0;
+        T1CONbits.TON = 0;
+        IEC0bits.T1IE = 0;
 
+        lcd_clear();
         lcd_locate(0, 0);
-        lcd_printf("Errors: %d", error);
+        lcd_printf("Error: %d", error);
 
         if (timeout == 1) {
-            uart2_putc(0);
             error++;
             timeout = 0;
+            uart2_putc(0);
 
             continue;
         }
 
         if ((buffer1[1] != ((crc >> 8) & 0xFF)) || (buffer1[2] != (crc & 0xFF))) {
-            uart2_putc(0);
             error++;
+            buffer2[0] = 0;
 
+            uart2_putc(0);
             continue;
         }
 
+        lcd_locate(0, 1);
+        lcd_printf("CRC: %x", crc);
+        lcd_locate(0, 2);
+        lcd_printf("Message: %s", buffer2);
+
         uart2_putc(1);
         buffer2[length] = 0;
-        //lcd_locate(0, 1);
-        //lcd_printf("Message: %s", buffer2);
+        error = 0;
+        timeout = 0;
     }
-    return(0);
+    return (0);
 }
