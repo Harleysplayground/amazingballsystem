28dee6c4d6b39c57e0775643a649ab399279e979
mwallic2 <mwallic2@a2f417b5-1eaa-4f03-aa2b-86c8419867a1>
2014-10-30 21:43:04 +0000

    flexmotor online

---
+++ _projects/groupAB3_1/lab05/flexmotor.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   flexmotor.h
+ * Author: mwallic2
+ *
+ * Created on October 23, 2014, 2:31 PM
+ */
+
+#ifndef FLEXMOTOR_H
+#define	FLEXMOTOR_H
+
+void motor_set_duty (int chan, int duty_us);
+void motor_init (int chan);
+
+
+#endif	/* FLEXMOTOR_H */
+
